c727d3bacb12ab14ceee0244d0506f2b0f8a4469
Matt Tobin <matthewtobin@outlook.com>
2015-12-06 16:09:20 +0000

    Update folder structure
    Seperate src and dist assets

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,32 +6,32 @@ var uglify = require('gulp-uglify');
 
 // SASS
 gulp.task('sass', function() {
-	gulp.src('assets/scss/*.scss')
+	gulp.src('assets/src/scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('assets/css'));
+		.pipe(gulp.dest('assets/src/css'));
 });
 
 // Minify CSS
 gulp.task('minify-css', function() {
-	return gulp.src('assets/css/style.css')
+	return gulp.src('assets/src/css/style.css')
 		.pipe(minifyCss())
-		.pipe(rename('assets/css/style.min.css'))
-		.pipe(gulp.dest(''));
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('assets/dist/css'));
 });
 
 // Minify JS
 gulp.task('minify-js', function() {
-	return gulp.src('assets/js/main.js')
+	return gulp.src('assets/src/js/main.js')
 		.pipe(uglify())
-		.pipe(rename('assets/js/main.min.js'))
-		.pipe(gulp.dest(''));
+		.pipe(rename('main.min.js'))
+		.pipe(gulp.dest('assets/dist/js'));
 });
 
 // Watch
 gulp.task('watch', function() {
-	gulp.watch('assets/scss/*.scss', ['sass']);
-	gulp.watch('assets/css/style.css', ['minify-css']);
-	gulp.watch('assets/js/main.js', ['minify-js']);
+	gulp.watch('assets/src/scss/*.scss', ['sass']);
+	gulp.watch('assets/src/css/style.css', ['minify-css']);
+	gulp.watch('assets/src/js/main.js', ['minify-js']);
 });
 
 gulp.task('default', ['sass', 'minify-css', 'minify-js', 'watch']);
